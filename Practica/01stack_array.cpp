a641bf48dcacf2704f59964c42a8db72bc4e6c01
Sebastián Sánchez <seb2001sac@gmail.com>
2024-03-22 12:55:43 -0500

    v1.0.0 Funcional

---
+++ Practica/01stack_array.cpp
@@ -0,0 +1,73 @@
+/*
+    Crear una pila (stack) del tipo array que tenga push() y pop() como metodos.
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Stack {
+    int array[10];
+    int* top;
+    int* lastItem;
+
+    Stack();
+    bool push(int);
+    bool pop(int &);
+    void print();
+
+};
+
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+
+int main()
+{
+    Stack stack1;
+    
+    for (int i = 0; i < 10; i++)
+        stack1.push(i);
+    stack1.print();
+
+    for (int i = 0, x; i < 3; i++) {
+        stack1.pop(x);
+        cout << "Popped: " << x << endl;
+    }
+    stack1.print();
+
+    for (int i = 20; i < 25; i++)
+        stack1.push(i);
+    stack1.print();
+
+    return 0;
+}
+
+//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+
+Stack::Stack() {
+    top = nullptr;
+    lastItem = array + 10 - 1;
+}
+
+bool Stack::push(int value) {
+    if (top == lastItem)
+        return false;
+    top == nullptr ? top = array : top++;
+    *top = value;
+    return true;
+}
+
+bool Stack::pop(int &x) {
+    if (top == nullptr)
+        return false;
+    x = *top;
+    top == array ? top = nullptr : top--;
+    return true;
+}
+
+void Stack::print() {
+    cout << "Stack --> [ ";
+    for (int* a = array; a <= lastItem; a++)
+        cout << *a << " ";
+    cout << "]" << endl;
+}
