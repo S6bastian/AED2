6dc85c2df0f5c4e0a4bb36f1cbb39368d794c86d
Sebastián Sánchez <seb2001sac@gmail.com>
2024-04-25 18:44:57 -0500

    v1.0.0 Finalizado

--- Teoria/05COrderedList/COrderedList_FO_T.cpp
+++ Teoria/05COrderedList/COrderedList_FO_T.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 template<class T>
+struct Greater {
+    bool operator()(T a, T b) {
+        return a > b;
+    }
+};
+
+template<class T>
+struct Less {
+    bool operator()(T a, T b) {
+        return a < b;
+    }
+};
+
+template<class T>
 struct Node {
     T value;
     Node<T>* next;
@@ -10,44 +24,70 @@ struct Node {
     Node(T, Node<T>*);
 };
 
-template<class T>
+template<class T, class C>
 struct OrderedList {
     Node<T>* head;
 
     OrderedList();
-    bool find(T, Node<T>**&);
+    bool find(T, Node<T>**&, C);
     bool insert(T);
     bool remove(T);
     void print();
 };
+
+
 
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
 
 
 int main() {
-    OrderedList<int> list;
-    int i = 0;
-    
-    for (int i = 0; i < 12; i += 2)
-        list.insert(i);
 
-    list.print();
-
-    for (int i = 0; i < 18; i += 3)
-        list.insert(i);
-
-    list.print();
-
-    for (int i = 0; i < 18; i += 3)
-        list.remove(i);
-
-    list.print();
+    cout << "OrderedList using Greater functor" << endl;
+    OrderedList<int, Greater<int>> list_greater;
 
     for (int i = 0; i < 12; i += 2)
-        list.remove(i);
+        list_greater.insert(i);
 
-    list.print();
+    list_greater.print();
+
+    for (int i = 0; i < 18; i += 3)
+        list_greater.insert(i);
+
+    list_greater.print();
+
+    for (int i = 0; i < 18; i += 3)
+        list_greater.remove(i);
+
+    list_greater.print();
+
+    for (int i = 0; i < 12; i += 2)
+        list_greater.remove(i);
+
+    list_greater.print();
+
+    cout << endl << "OrderedList using Less functor" << endl;
+    OrderedList<int, Less<int>> list_less;
+
+    for (int i = 0; i < 12; i += 2)
+        list_less.insert(i);
+
+    list_less.print();
+
+    for (int i = 0; i < 18; i += 3)
+        list_less.insert(i);
+
+    list_less.print();
+
+    for (int i = 0; i < 18; i += 3)
+        list_less.remove(i);
+
+    list_less.print();
+
+    for (int i = 0; i < 12; i += 2)
+        list_less.remove(i);
+
+    list_less.print();
 
     return 0;
 }
@@ -69,33 +109,35 @@ Node<T>::Node(T v, Node<T>* n) {
 
 //*********************************************************************************************
 
-template<class T>
-OrderedList<T>::OrderedList() {
+template<class T, class C>
+OrderedList<T, C>::OrderedList() {
     head = nullptr;
 }
 
-template<class T>
-bool OrderedList<T>::find(T value, Node<T>**& p) {
-    for (p = &head; *p && (*p)->value < value; p = &(*p)->next);
+template<class T, class C>
+bool OrderedList<T, C>::find(T value, Node<T>**& p, C FO) {
+    for (p = &head; *p && FO((*p)->value, value); p = &(*p)->next);
     return *p && (*p)->value == value;
 }
 
-template<class T>
-bool OrderedList<T>::insert(T value) {
+template<class T, class C>
+bool OrderedList<T, C>::insert(T value) {
     Node<T>** p = nullptr;
+    C FO;
 
-    if (find(value, p))
+    if (find(value, p, FO))
         return false;
 
     *p = new Node<T>(value, *p);
     return true;
 }
 
-template<class T>
-bool OrderedList<T>::remove(T value) {
+template<class T, class C>
+bool OrderedList<T, C>::remove(T value) {
     Node<T>** p = nullptr;
+    C FO;
 
-    if (!find(value, p))
+    if (!find(value, p, FO))
         return false;
 
     Node<T>* tmp = *p;
@@ -104,8 +146,8 @@ bool OrderedList<T>::remove(T value) {
     return true;
 }
 
-template<class T>
-void OrderedList<T>::print() {
+template<class T, class C>
+void OrderedList<T, C>::print() {
     cout << "OrderedList";
     for (Node<T>** p = &head; *p; p = &(*p)->next)
         cout << "  -->  " << (*p)->value;
