ac0a479127aa74f7d5a8e1263067bd7e2fbc6756
Sebastián Sánchez <seb2001sac@gmail.com>
2024-06-21 18:38:44 -0500

    v1.1.0 Imprime en shell y correción de remove() en caso de quedar vacío

--- Teoria/08AVLTree/AVLTree.cpp
+++ Teoria/08AVLTree/AVLTree.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <stack>
+#include <list>
+#include <string>
+#include <iterator> // For std::prev
+#include <math.h>
+
 using namespace std;
 
 //Node
@@ -22,15 +27,17 @@ private:
 
     int getBalance(Node*);
     int updateHeight(Node*);
-    void balanceTree(Node**);   // only when getBalance() is 2 or -2
+    void balanceTree(Node**);   // Only when getBalance() is 2 or -2
     Node** getSuccessor(Node**, stack<Node**>&);
     void preorder(Node*);
+    void bfs(list<Node*>*&);    // Breadth First Search
 public:
     AVLTree();
     bool search(int, Node**&, stack<Node**>&);
     bool insert(int);
     bool remove(int);
     void print();
+    void printPreorder();
 };
 
 //---------------------------------------------------------------------------------------------
@@ -44,12 +51,31 @@ int main() {
         test1.insert(i);
         test1.print();
     }
-    cout << "Remove: " << endl;
+
+    //test1.print();
+
+    //test1.printPreorder();
+
+    cout << "Remove: " << 9 << endl;
     test1.remove(9);
     test1.print();
+    cout << "Remove: " << 12 << endl;
     test1.remove(12);
     test1.print();
-    test1.remove(15); //error
+    cout << "Remove: " << 15 << endl;
+    test1.remove(15);
+    test1.print();
+    cout << "Remove: " << 3 << endl;
+    test1.remove(3);
+    test1.print();
+    cout << "Remove: " << 6 << endl;
+    test1.remove(6);
+    test1.print();
+    cout << "Remove: " << 18 << endl;
+    test1.remove(18);
+    test1.print();
+    cout << "Remove: " << 0 << endl;
+    test1.remove(0);
     test1.print();
 
 }
@@ -120,7 +146,7 @@ void AVLTree::balanceTree(Node** parent) {
 
 Node** AVLTree::getSuccessor(Node** successor, stack<Node**>& path) {
     if ((*successor)->nodes[1] == nullptr)
-        return nullptr;
+        return nullptr; //&(*successor)->nodes[0]
 
     successor = &(*successor)->nodes[1];
     path.push(successor);
@@ -130,13 +156,41 @@ Node** AVLTree::getSuccessor(Node** successor, stack<Node**>& path) {
     }
     return successor;
 }
-
+//-----------------------POSIBLE ELIMINACION DE PREORDER
 void AVLTree::preorder(Node* root) {
     if (root == nullptr)
         return;
+    /*if (root->nodes[0]) {
+        
+
+        for (int i = 0; i < root->height; i++) {
+
+        }
+    }*/
     cout << root->value << " ";
     preorder(root->nodes[0]);
     preorder(root->nodes[1]);
+}
+
+void AVLTree::bfs(list<Node*>*& lvlNodes) {
+    list<Node*>* newList = new list<Node*>;
+
+    for (Node* current; !lvlNodes->empty(); lvlNodes->pop_front()) {
+        current = lvlNodes->front();
+        
+        if (current) {
+            newList->push_back(current->nodes[0]);
+            newList->push_back(current->nodes[1]);
+        }
+        else {
+            newList->push_back(nullptr);
+            newList->push_back(nullptr);
+        }
+        
+    }
+    delete lvlNodes;
+    lvlNodes = newList;
+
 }
 
 bool AVLTree::search(int value, Node**& current, stack<Node**>& path) {
@@ -170,13 +224,32 @@ bool AVLTree::insert(int value) {
 }
 
 bool AVLTree::remove(int value) {
+    if (!root) {
+        return 0;
+    }
+    if (root->nodes[0] == nullptr && root->nodes[1] == nullptr) {
+        delete root;
+        root = nullptr;
+        return 1;
+    }
     Node** current = nullptr;
     stack<Node**> path;
     if (!search(value, current, path)) {
         return 0;
     }
     Node** tmp = getSuccessor(current, path);
-    if (!*tmp) {
+
+    if (!tmp) {
+        Node* tmp1 = *current;
+        if (root == *current) {
+            root = root->nodes[0];
+        }
+        else
+            *current = (*current)->nodes[0];
+        delete tmp1;
+    }
+
+    else if (!*tmp) {
         Node* tmp1 = *current;
         *current = (*current)->nodes[0];
         delete tmp1;
@@ -206,6 +279,56 @@ bool AVLTree::remove(int value) {
 }
 
 void AVLTree::print() {
+    if (root == nullptr) {
+        cout << "Empty Tree" << endl;
+        return;
+    }
+
+    list<Node*>* lvlNodes = new list<Node*>;
+    lvlNodes->push_back(root);
+    cout << "-print()-" << endl;
+    int digits = 3;
+
+    for (int height = lvlNodes->front()->height, margin, space; height > 0; height--) {
+        
+        margin = (pow(2, height - 1) - 1) * digits;
+        for (int i = 0; i < margin; i++) {
+            cout << " ";
+        }
+
+        space = (pow(2, height) - 1) * 3;
+        for (list<Node*>::iterator place = lvlNodes->begin();
+
+            place != lvlNodes->end();
+            place++) {
+            string currentSpace = to_string(*place ? (*place)->value : 0);
+            int offSpace = digits - currentSpace.length();
+            for (; offSpace > 0; cout << " ", offSpace--);
+
+            if (*place)
+                cout << (*place)->value;
+            else
+                cout << "N";
+            
+            if (place != prev(lvlNodes->end())) {   // To avoid unnecessary spaces
+                for (int j = 0; j < space; j++) {
+                    cout << " ";
+                }
+            }
+        }
+        
+        cout << endl;
+
+        if (height != 1)
+            bfs(lvlNodes);  // updates node queue
+        
+    }
+
+    cout << endl;
+}
+
+void AVLTree::printPreorder() {
+    cout << "-printPreoder()-" << endl;
     preorder(root);
     cout << endl;
 }
